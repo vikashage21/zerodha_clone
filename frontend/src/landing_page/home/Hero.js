820af32b5fcda8e2f3661126a045637ce52c172b
vikash sharma <vk220783@gmail.com>
2026-04-04 14:18:05 +0530

    hero section ui done

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div>
-            <h1
-            >
-                hero component
-            </h1>
+        <div className='container'>
+            <div className="row text-center">
+                <img src='media/image/homeHero.png' className='mb-5' alt='hero img'></img>
+                <h1>invest in everything</h1>
+                <p>online platform to invert in stock , derivatives , mutual funds , and more</p>
+                <button className="btn btn-primary text-white p-2 fs-6" style={{ "width": "20%", "margin": "auto" }}>
+                    signup now
+                </button>
+            </div>
         </div>
     )
 }
