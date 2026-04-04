4b3273c4dc92649f24b66e0ffbe4bc63aad298c0
vikash sharma <vk220783@gmail.com>
2026-04-04 15:42:34 +0530

    stats component ui - done

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -1,15 +1,48 @@
 import React from 'react'
 
 const Stats = () => {
-  return (
-    <div>
+    return (
+        <div className="container mt-2 p-5">
+            <div className="row p-5">
+                <div className="col-6 p-5">
+                    <h1 className="fs-2 mb-3">
+                        Trust with confidence
+                    </h1>
+                    <h2 className="fs-5 mb-3">
+                        Customer-first always
+                    </h2>
+                    <p className="text-muted">
+                        That's why 1.6+ crore customers trust Zerodha with ~ ₹6 lakh crores of equity investments, making us India’s largest broker; contributing to 15% of daily retail exchange volumes in India.
+                    </p>%No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like. Our philosophies.
+                    <h2 className="fs-5 mb-3">
+                        No spam or gimmicks
+                    </h2>
+                    <p className="text-muted">
+                        No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like. Our philosophies.
+                    </p>
+                    <h2 className="fs-5 mb-3">
+                        No spam or gimmicks
+                    </h2>
+                    <p className="text-muted">
+                        No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like. Our philosophies.
+                    </p>
 
-        <h1>
-            stats component
-        </h1>
-      
-    </div>
-  )
+
+
+                </div>
+                <div className="col-6 p-5 text-center">
+                    <img src="media/image/ecosystem.png" style={{ width: "90%" }} alt="state img" />
+                    <a href="#" className="mx-2" style={{textDecoration:"none"}}>Explore our product <i class="fa-solid fa-arrow-right"></i> </a>
+                    <a href="#" style={{textDecoration:"none"}} > Try kite demo <i class="fa-solid fa-arrow-right"></i></a>
+
+
+                </div>
+                        <img src="media/image/pressLogos.png" className='mt-5 mx-auto' alt="press img" style={{width :"50%"}}/>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Stats
